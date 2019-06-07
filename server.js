67ecb4ddb5bdb1fb1db7894e705571280846e8bd
Jake <jake.huggins@solirius.com>
2019-06-07 11:51:16 +0100

    add database configuration to server.js

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
+const dbConfig = require('./config/database.config');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(()=> {
+    console.log('successfully connected to the database');
+}).catch(error => {
+    console.log('could not connect ', error);
+    process.exit();
+});
 
 const port = process.env.PORT || 3000;
 
