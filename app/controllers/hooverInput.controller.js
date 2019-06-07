c6dfd88e5a7353e6e3e7ece37aa0f0a8e1dcbd46
Jake <jake.huggins@solirius.com>
2019-06-07 11:58:57 +0100

    validate incoming request for missing attributes

--- app/controllers/hooverInput.controller.js
+++ app/controllers/hooverInput.controller.js
@@ -1,4 +1,11 @@
 
 exports.clean = (req, res) => {
 
+    if (!req.body.roomSize || !req.body.coords
+        || !req.body.patches || !req.body.instructions) {
+        return res.status(400).send({
+            message: 'payload was missing an attribute'
+        });
+    }
+
 };
