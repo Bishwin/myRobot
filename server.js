484d46774edb45dfa6853a4a87fb892b8a1d50c7
Jake <jake.huggins@solirius.com>
2019-06-07 11:47:12 +0100

    setup bodyParser in server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const bodyParser = require('body-parser');
 const express = require('express');
-
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
 
